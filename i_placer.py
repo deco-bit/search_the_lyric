eeb3e04e5b425fd988d53b5da8f4d4c2e0834fde
AlienaNebula <alienanebula@gmail.com>
2019-12-17 13:26:40 +0500

    Places inverted index of each word in its corresponding location in directory designated for inverted index

---
+++ i_placer.py
@@ -0,0 +1,14 @@
+from i_indexer import inverted_indexer
+from helper import *
+
+while 1 == 1:
+    dir_dict_i = get_sub_dir_of_findex(docs_subdir_path)
+    inverted_batch = {}
+    for key in dir_dict_i:
+        f_index = f_index_main_dir + "\\" + dir_dict_i[key] + "\\" + key
+        inverted_batch = inverted_indexer(f_index)
+
+        for word in inverted_batch:
+            full_hashed_address, hashed_path, restricted = get_hashed_directory(i_index_dir, word, 255)
+            check_for_path(hashed_path)
+            output_on_hashed_path(inverted_batch, full_hashed_address, word, restricted)
